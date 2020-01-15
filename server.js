e6de33b20b5c5a33fba8194b32ac110479db874c
Ian 'Uther' Lawson <ian.lawson@redhat.com>
2020-01-15 09:19:00 +0000

    Corrected landing page

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 // Comment for git testing again
 app.get('/', function (req, res)
 {
-  console.log( "Request received, serving frontpage....");
-  res.render('frontpage.html');
+  console.log( "Request received, serving index....");
+  res.render('index.html');
 });
 
 app.get('/containerip', function (req,res)
